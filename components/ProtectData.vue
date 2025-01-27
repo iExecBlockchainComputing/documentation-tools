ae7c953f231cd9e48c62c4eedd39c0d3ee406f20
Le-Caignec <robinlecaignec@yahoo.fr>
2025-01-27 10:32:50 +0100

    Remove border-left styles from solution and success notes in multiple components to be consistent with the others already existing

--- components/ProtectData.vue
+++ components/ProtectData.vue
@@ -198,7 +198,6 @@ input:disabled {
   padding: 0.75rem 1rem;
   border-radius: var(--border-radius);
   font-size: 0.9rem;
-  border-left: 4px solid rgb(255, 59, 48);
 }
 
 .success-note {
@@ -212,7 +211,6 @@ input:disabled {
   padding: 1.5rem;
   text-align: center;
   margin-top: 2rem;
-  border-left: 4px solid rgb(52, 199, 89);
 }
 
 .success-header {
