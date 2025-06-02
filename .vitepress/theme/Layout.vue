2bc748627dc641953f69f81647b793bbf12bb2fb
Le-Caignec <robinlecaignec@yahoo.fr>
2025-06-02 13:07:38 +0200

    fix: Correct import case for ChatGPTButton component

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -1,6 +1,6 @@
 <script setup>
 import DefaultTheme from 'vitepress/theme';
-import ChatGptButton from './ChatGptButton.vue';
+import ChatGptButton from './ChatGPTButton.vue';
 
 const { Layout } = DefaultTheme;
 </script>
