74e5ff982f59881843bfe1f54473697fb65507fe
Robin Le Caignec <72495599+Le-Caignec@users.noreply.github.com>
2025-06-12 12:18:06 +0200

    feat: Add chatgpt integration (#289)

---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,14 @@
+<script setup>
+import DefaultTheme from 'vitepress/theme';
+import ChatGPTButton from './ChatGPTButton.vue';
+
+const { Layout } = DefaultTheme;
+</script>
+
+<template>
+  <Layout>
+    <template #aside-outline-before>
+      <ChatGPTButton />
+    </template>
+  </Layout>
+</template>
